5b13af57826c2780d95cfe37d77ab4030aeeea76
Yazan <yazanmatar90@gmail.com>
2020-04-26 20:02:25 -0400

    brain struct

--- Tipsy/Model/Brain.swift
+++ Tipsy/Model/Brain.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2020 The App Brewery. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct Brain {
+    var tipPercentage:Int? = 10
+    var splitCount:Int? = 2
+    var billTotal:Float?
+    var totalPerPerson:Float?
+    
+    func getTipPercenetage() -> Int {
+        return tipPercentage ?? 0
+    }
+    func getSplitCount() -> Int {
+        return splitCount ?? 0
+    }
+    mutating func setTipPercentage( per percentage:Int) {
+        tipPercentage = percentage
+    }
+    mutating func setSplitCount(count:Int) {
+        splitCount = count
+    }
+    mutating func setBillTotal(total:Float) {
+        billTotal = total
+    }
+    mutating func calculateTotalPerPerson(){
+        if tipPercentage != nil && splitCount != nil && billTotal != nil {
+            totalPerPerson = (billTotal! + ((billTotal! * 10) / 100)) / Float(splitCount!)
+        }
+    }
+    func getTotalPerPerson() -> Float {
+        return totalPerPerson ?? 0
+    }
+    
+}
