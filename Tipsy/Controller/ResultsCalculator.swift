bc9c3778fb28f08e3a6f192ac344d3eb302bf5b6
Yazan <yazanmatar90@gmail.com>
2020-04-26 20:02:02 -0400

    created Brain struct

--- Tipsy/Controller/ResultsCalculator.swift
+++ Tipsy/Controller/ResultsCalculator.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 The App Brewery. All rights reserved.
 //
 
-import Foundation
+import UIKit
+class ResultsCalculator:UIViewController {
+    var totalPerPerson:Float?
+    var splitCount:Int?
+    var tipPercentage:Int?
+    @IBOutlet weak var totalPerPersonLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        totalPerPersonLabel.text = String(format:"%.2f",totalPerPerson ?? 0)
+           descriptionLabel.text = "Split between \(splitCount ?? 0) people, with \(tipPercentage ?? 0)% tip."
+     
+    }
+    @IBAction func recalculateButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
